5f3c2437d19ba4b1033938e32e262330f2a9ac74
aDevMister <herbdolqdus@gmail.com>
2024-10-09 21:58:16 +0100

    first

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,14 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
       <Image
+      
         src={bg}
         alt="background-image"
         className="w-full h-full object-cover object-center opacity-25"
       />
-      <div className="w-full h-screen"></div>
+      <div className="w-full h-screen">
+
+      </div>
     </main>
   );
 }
