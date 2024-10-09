5c8be3bd422a650a948c4a4aa7784e75f244cb6c
aDevMister <herbdolqdus@gmail.com>
2024-10-09 21:52:46 +0100

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
-import bg from "../../public/background/home-background.jpeg"
+import bg from "../../public/background/home-background.jpeg";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <Image src={bg} alt="background-image" className="w-full h-full object-cover object-center opacity-25" />
-      <div className="w-full h-screen">
-          
-      </div>
+      <Image
+        src={bg}
+        alt="background-image"
+        className="w-full h-full object-cover object-center opacity-25"
+      />
+      <div className="w-full h-screen"></div>
     </main>
   );
 }
